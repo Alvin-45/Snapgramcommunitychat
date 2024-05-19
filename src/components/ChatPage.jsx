66ef5f4f640018b86981be1021e73e5ae4b287e5
Alvin-45 <webshieldhelp@gmail.com>
2024-05-20 01:35:14 +0530

    final commit

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -5,6 +5,7 @@ import Msgbox from './Msgbox';
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
 import { setChat } from "../Redux/chatSlice";
+import Spinner from 'react-bootstrap/Spinner';
 
 function ChatPage() {
   
@@ -16,14 +17,16 @@ function ChatPage() {
   const boxref = useRef(null);
   const dispatch = useDispatch();
   const datared = useSelector(state => state.chatReducer);
+  const [connected, setConnected] = useState(false);
 
   useEffect(() => {
-    const socket = io("http://localhost:3001");
+    const socket = io("https://cboatserver.onrender.com");
     setNewSocket(socket);
 
     socket.on("connect", () => {
       setUserId(socket.id);
       socket.emit('joined', { user });
+      setConnected(true);
     });
 
     socket.on('welcome', (data) => {
@@ -62,7 +65,7 @@ function ChatPage() {
   return (
     <>
       <div className=" bg-dark">
-        <div className="row chatpage  shadow d-flex justify-content-center align-items-center w-100">
+        {connected?(<div className="row chatpage  shadow d-flex justify-content-center align-items-center w-100">
 
           
             <div className="col chatborder text-light bg-success p-3 w-100">
@@ -76,14 +79,18 @@ function ChatPage() {
               ))}
   
               
-            </div>
+            </div>:
+           
             <div className="textmsg w-100  border-top pt-3" style={{overflowY:'hidden'}}>
                 <input type="text" className="rounded inpbx w-75" placeholder='Type Message...' onChange={(e) => setTypeMessage(e.target.value)} value={typeMessage} style={{ height: '40px' }} />
                 <button onClick={send} className='btn btn-success snt' style={{ width: '10%', height: '40px' }}><i className="fa-regular fa-paper-plane" style={{ color: "#fcfcfc" }}></i></button>
               </div>
           </div>
 
-        </div>
+        </div>): (<div className="noconnection d-flex justify-content-center align-items-center w-100" style={{height:'100vh'}}>
+              <Spinner animation="border" variant="primary" style={{zIndex:'9999999999999999'}}/> <span className="fw-bolder text-primary">Connecting to server(max:20s)</span>
+
+            </div>)}
         <div className="watermark w-100" style={{position:'absolute',marginTop:'-60px',marginLeft:'91%'}}>
           <h3 className='title text-light fixed-end' style={{zIndex:'99999'}}>SnapGram</h3>
         </div>
